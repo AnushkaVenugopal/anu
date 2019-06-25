39f3868d8cfb5c0f98aac0b3d328c786cac8c5ae
AnushkaVenugopal <52197939+AnushkaVenugopal@users.noreply.github.com>
2019-06-25 19:50:45 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+<!Doctype html>
+<html>
+<body>
+<form action="/calc" method="post">
+Student_name<input type="text" name="name"/><br>
+Subject<input type="text" name="subject"/><br>
+Marks<input type="text" name="marks"/><br>
+<input type ="button" name="button" id="btn">Submit</input>
+</form>
+</body>
+</html>
